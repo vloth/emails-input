1418a18fe533c3398b01f8a973cf072eca5db91d
vloth <fran.fhb@gmail.com>
2020-07-21 19:41:20 +0200

    ϋ refactor: Create utils module

---
+++ src/js/lib/utils.js
@@ -0,0 +1,39 @@
+var lib = window.lib || { }
+
+lib.utils = (function() {
+  'use strict'
+
+  function randomEmail() { 
+    return randomText(randomNum(3, 7)) + '@' + randomText(randomNum(2, 8)) + '.com'
+  }
+
+  function randomText(len) { 
+    let str = ''
+    const rand='abcdefghijklmnopqrstuvwxyz123456789'
+ 
+    for (let i=0; i < len; i++) { 
+      str += rand.charAt(Math.round(rand.length * Math.random()))
+    }
+ 
+    return str
+  }
+
+  function randomNum(min, max) { 
+    return Math.floor(Math.random() * max) + min
+  }
+
+  function makeEventListenerFactory(element) {
+    const handlers = []
+    function addEventListener(event, eventHandler) {
+      const handler = element.addEventListener(event, eventHandler)
+      handlers.push({ event: event, handler: handler })
+    }
+    return { handlers: handlers, addEventListener: addEventListener }
+  }
+
+  return {
+    random: { email: randomEmail },
+    dom: { makeEventListenerFactory: makeEventListenerFactory }
+  }
+
+}())
